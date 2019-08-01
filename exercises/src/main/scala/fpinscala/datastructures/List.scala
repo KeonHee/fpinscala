8ffb5e9db4bde71f937490e2a859f35e1a281580
KeonHee <kimkh6554@gmail.com>
2019-08-01 19:02:33 +0900

    Solve to chapter 2~3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -8,61 +8,152 @@ which may be `Nil` or another `Cons`.
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  }
+  def sum(ints: List[Int]): Int =
+    ints match { // A function that uses pattern matching to add up a list of integers
+      case Nil => 0 // The sum of the empty list is 0.
+      case Cons(x, xs) =>
+        x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    }
 
   def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
+    case Nil          => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs)  => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _)))          => x
+    case Nil                                   => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
+    case Cons(h, t)                            => h + sum(t)
+    case _                                     => 101
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
-      case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
-    }
-
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+      case Nil        => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil        => Nil
+      case Cons(_, t) => t
+    }
 
-  def tail[A](l: List[A]): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil        => Cons(h, Nil)
+      case Cons(_, t) => Cons(h, t)
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n == 0) {
+      l
+    } else {
+      l match {
+        case Nil                 => Nil
+        case Cons(_, t) if n > 0 => drop(t, n - 1)
+        case _                   => l
+      }
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop2[A](l: List[A], n: Int): List[A] =
+    l match {
+      case Nil                 => Nil
+      case Cons(_, t) if n > 0 => drop(t, n - 1)
+      case _                   => l
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case _                  => l
+    }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Cons(h, t) =>
+        t match {
+          case Nil => Nil
+          case _   => Cons(h, init(t))
+        }
+      case _ => Nil
+    }
 
-  def length[A](l: List[A]): Int = ???
+  def init2[A](l: List[A]): List[A] =
+    l match {
+      case Cons(_, Nil) => Nil
+      case Cons(h, t)   => Cons(h, init(t))
+    }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, b) => 1 + b)
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil         => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match {
+      case Nil        => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
+
+  def sumWithFoldLeft(ints: List[Int]): Int =
+    foldLeft(ints, 0)(_ + _)
+
+  def productWithFoldLeft(ds: List[Double]): Double =
+    foldLeft(ds, 1.0)(_ * _)
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List[A]())((z: List[A], r: A) => Cons(r, z))
+
+  def foldRightWithFoldLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(as, (b: B) => b)((g, a) => b => g(a, b))(z)
+
+  def foldLeftWithFoldRight[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(l, (b: B) => b)((a, g) => b => g(a, b))(z)
+
+  def map[A, B](l: List[A])(f: A => B): List[B] =
+    foldLeft(l, List[B]())((b: List[B], a: A) => Cons(f(a), b))
+
+  def main(args: Array[String]): Unit = {
+    println(x) // x == 3
+
+    println(tail(List(1, 2, 3, 4, 5)))
+
+    println(setHead(List(1, 2, 3, 4, 5), 6))
+
+    println(drop(List(1, 2, 3, 4, 5), 3))
+    println(drop2(List(1, 2, 3, 4, 5), 3))
+
+    println(dropWhile(List(1, 2, 3, 4, 5), (_: Int) < 3))
+
+    println(init(List(1, 2, 3, 4, 5)))
+    println(init2(List(1, 2, 3, 4, 5)))
+
+    println(length(List(1, 2, 3, 4, 5)))
+
+    println(foldLeft(List(1, 2, 3, 4, 5), 6)((a, b) => a + b))
+
+    println(sumWithFoldLeft(List(1, 2, 3, 4, 5)))
+
+    println(productWithFoldLeft(List(1, 2, 3, 4, 5)))
+
+    println(reverse(List(1, 2, 3, 4, 5)))
+  }
+
 }
--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -1,5 +1,7 @@
 package fpinscala.gettingstarted
 
+import scala.annotation.tailrec
+
 // A comment!
 /* Another comment */
 /** A documentation comment */
@@ -21,7 +23,7 @@ object MyModule {
     @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
-      else go(n-1, n*acc)
+      else go(n - 1, n * acc)
 
     go(n, 1)
   }
@@ -30,13 +32,24 @@ object MyModule {
   def factorial2(n: Int): Int = {
     var acc = 1
     var i = n
-    while (i > 0) { acc *= i; i -= 1 }
+    while (i > 0) {
+      acc *= i;
+      i -= 1
+    }
     acc
   }
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @tailrec
+    def go(a: Int, b: Int, n: Int): Int =
+      if (n == 0) a
+      else go(b, a + b, n - 1)
+
+    go(0, 1, n)
+  }
+
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -71,7 +84,9 @@ object TestFib {
   // test implementation of `fib`
   def main(args: Array[String]): Unit = {
     println("Expected: 0, 1, 1, 2, 3, 5, 8")
-    println("Actual:   %d, %d, %d, %d, %d, %d, %d".format(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5), fib(6)))
+    println(
+      "Actual:   %d, %d, %d, %d, %d, %d, %d"
+        .format(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5), fib(6)))
   }
 }
 
@@ -79,6 +94,7 @@ object TestFib {
 // convenient to have syntax for constructing a function
 // *without* having to give it a name
 object AnonymousFunctions {
+
   import MyModule._
 
   // Some examples of anonymous functions:
@@ -89,7 +105,10 @@ object AnonymousFunctions {
     println(formatResult("increment2", 7, (x) => x + 1))
     println(formatResult("increment3", 7, x => x + 1))
     println(formatResult("increment4", 7, _ + 1))
-    println(formatResult("increment5", 7, x => { val r = x + 1; r }))
+    println(formatResult("increment5", 7, x => {
+      val r = x + 1;
+      r
+    }))
   }
 }
 
@@ -107,12 +126,13 @@ object MonomorphicBinarySearch {
       else {
         val mid2 = (low + high) / 2
         val d = ds(mid2) // We index into an array using the same
-                         // syntax as function application
+        // syntax as function application
         if (d == key) mid2
-        else if (d > key) go(low, mid2, mid2-1)
+        else if (d > key) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
+
     go(0, 0, ds.length - 1)
   }
 
@@ -122,7 +142,7 @@ object PolymorphicFunctions {
 
   // Here's a polymorphic version of `binarySearch`, parameterized on
   // a function for testing whether an `A` is greater than another `A`.
-  def binarySearch[A](as: Array[A], key: A, gt: (A,A) => Boolean): Int = {
+  def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
     @annotation.tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
       if (low > high) -mid - 1
@@ -130,36 +150,44 @@ object PolymorphicFunctions {
         val mid2 = (low + high) / 2
         val a = as(mid2)
         val greater = gt(a, key)
-        if (!greater && !gt(key,a)) mid2
-        else if (greater) go(low, mid2, mid2-1)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
+
     go(0, 0, as.length - 1)
   }
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    def go(as: Array[A], n: Int): Boolean =
+      if (as.length - 1 <= n) true
+      else if (!gt(as(n), as(n + 1))) false
+      else go(as, n + 1)
+    go(as, 0)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
   // Exercise 3: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A, B, C](f: (A, B) => C): A => B => C =
+    (a: A) => (b: B) => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a:A, b: B) => f(a)(b)
+
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -169,10 +197,10 @@ object PolymorphicFunctions {
   and uncurry and the two forms are in some sense "the same". In FP jargon,
   we say that they are _isomorphic_ ("iso" = same; "morphe" = shape, form),
   a term we inherit from category theory.
-  */
+   */
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    (a: A) => f(g(a))
 }
